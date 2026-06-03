4e89262cb86134ab4324f59b59a844069dbc3c37
jothams-onepoint <85735442+jothams-onepoint@users.noreply.github.com>
2026-06-03 14:26:04 +0100

    Update streamlit_app.py
    Fix request URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)        
 st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
